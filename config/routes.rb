c41ba3e8acee14b1c562dfda925eb5216119e446
lvjiiiiu <lv_jiiiiu@yahoo.co.jp>
2021-05-18 15:27:51 +0900

    保存

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :books
+  resources :author
+  resources :publishers
+  resources :book_authors
 end
