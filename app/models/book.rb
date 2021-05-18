5b18f6257245b80cfafa0690ca44e2d011c81e1b
lvjiiiiu <lv_jiiiiu@yahoo.co.jp>
2021-05-18 14:33:44 +0900

    リレーション

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,4 +3,6 @@ class Book < ApplicationRecord
   # Book.costly は　Book.where("price>?", 3000)と等価
   scope :written_about, -> (theme){ where("name like?", "%#{theme}%")}
   scope :find_price, -> (price){find_by(price:price)}
+
+  belongs_to :publisher
 end
